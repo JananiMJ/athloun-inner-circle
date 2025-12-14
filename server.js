e6bd18561c2ea85afc7a3f643574000fccd2d4ce
JananiMJ <74947893+JananiMJ@users.noreply.github.com>
2025-12-15 02:00:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -55,9 +55,15 @@ const Member = mongoose.model('Member', memberSchema, 'members');
 // ===== SENDGRID =====
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
